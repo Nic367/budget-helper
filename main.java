03239804f1fc60fefdedf98ae06abda132472278
Nic367 <nruizto1@binghamton.edu>
2022-02-09 01:02:15 -0500

    Pausing and trying out a log in page first, then continuing down my thought out stuff

--- main.java
+++ main.java
@@ -4,19 +4,14 @@ import javax.swing.*;
 import java.awt.event.*;
 
 public class main extends JFrame implements ActionListener{
-    //javac *.java
-    //java main.java
-    
     public static final int WIDTH = 800;
     public static final int HEIGHT = 400;
     
-    private JTextField hourlyIoField, hoursIoField, salaryIoField, debtIoField, debtPayIoField, qsIoField;  //TODO: remove last
-    private JRadioButton aButton, bButton;  //TODO: rename these to their proper names
-    private JCheckBox cButton;
+    private JTextField usernameText, passwordText;
+    private JButton loginButton, registerButton, resetButton;
     
     public static void main(String[] args){
         main t1 = new main();
-        //t1.pack();
         t1.setVisible(true);
     }
     
@@ -26,72 +21,18 @@ public class main extends JFrame implements ActionListener{
         setSize(WIDTH, HEIGHT);                     /*instead of "t1.pack();" in main*/
         
         Container contentPane = getContentPane();
-        contentPane.setLayout(new GridLayout(1,1));
-        
-//SETTINGS PAGE		
-		JPanel settingsPanel = new JPanel();                /*filled row by row, and from left to right*/
-        settingsPanel.setLayout(new GridLayout(2,3));		/*row, col*/
-        //TEXT PANEL
-		JPanel qsTextPanel = new JPanel();
-		qsTextPanel.setLayout(new BorderLayout());
-		qsIoField = new JTextField();				        /*defined private*/
-		qsIoField.setBackground(Color.white);
-        //ADD TO TEXT PANEL
-		qsTextPanel.add(qsIoField, BorderLayout.CENTER);
-		//BUTTON PANEL
-		JPanel qsButtonsPanel = new JPanel();
-		qsButtonsPanel.setLayout(new FlowLayout());
-		
-		aButton = new JRadioButton("Hourly");
-        bButton = new JRadioButton("Salary");
-        bButton.setSelected(true);
-        
-        cButton = new JCheckBox("Debt");
-        cButton.setSelected(false);
-        
-		
-		aButton.addActionListener(this);
-		bButton.addActionListener(this);
-		cButton.addActionListener(this);
-		//ADD TO BUTTON PANEL
-		qsButtonsPanel.add(aButton);
-		qsButtonsPanel.add(bButton);
-		qsButtonsPanel.add(cButton);
-		
-		
-//ADD TO QUESTION PAGE
-        settingsPanel.add(qsTextPanel);
-        settingsPanel.add(qsButtonsPanel);
         
         
         
         
         
         
-        
-        
-        
-        
-        
-        
-        //ADD PAGES/PANELS TO CONTENT PANE
         contentPane.add(settingsPanel);
     }
     
     public void actionPerformed(ActionEvent e){
-        if(e.getActionCommand().equals("Hourly") || e.getActionCommand().equals("Salary")){
-            if(e.getActionCommand().equals("Hourly")){
-            	aButton.setSelected(true);
-            	bButton.setSelected(false);
-            }else{
-                aButton.setSelected(false);
-            	bButton.setSelected(true);
-            }
+        if(e.getActionCommand().equals("")){
+        	//qsIoField.setText("That's an L, try again.");
         }
-        
-        if(e.getActionCommand().equals("D")){
-        	qsIoField.setText("D");
-        }else 
-            qsIoField.setText("That's an L, try again.");
     }
 }
